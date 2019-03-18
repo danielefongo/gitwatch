8633ec330cc136cbba8de82432cd00834373bd06
danielefongo <danielefongo@gmail.com>
2019-03-18 14:17:25 +0100

    fix popup showing in push needed

--- check.sh
+++ check.sh
@@ -3,8 +3,16 @@
 update="N"
 
 while true; do
+
   git fetch
-  if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
+  UPSTREAM=${1:-'@{u}'}
+  LOCAL=$(git rev-parse @)
+  REMOTE=$(git rev-parse "$UPSTREAM")
+  BASE=$(git merge-base @ "$UPSTREAM")
+
+  if [ $LOCAL = $REMOTE ]; then
+    update="N"
+  elif [ $LOCAL = $BASE ]; then
     if [[ $update = "N" ]]; then
       osascript -e 'display notification "Remote branch updated" with title "GitWatch Daemon"'
       update="Y"
