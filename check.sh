75ddb304372dd781a3a4df79da19f2c676fe9098
danielefongo <danielefongo@gmail.com>
2019-03-18 13:38:13 +0100

    first commit

---
+++ check.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+update="N"
+
+while true; do
+  git fetch
+  if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
+    if [[ $update = "N" ]]; then
+      osascript -e 'display notification "Remote branch updated" with title "Git Daemon"'
+      update="Y"
+    fi
+  else
+    update="N"
+  fi
+
+  sleep 10
+done
